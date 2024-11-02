dbd8ea9e6bc510d70cd4f6beedc891b5eafd2cbe
AspenTree <719391@student.dusd.net>
2024-11-01 22:37:10 -0700

    work from home, nov 1
    almost finished health, need a ui for it.  added enemy, expanded world, added hitboxes for stuff, added healing.  Working on animation next.

--- 2DBeginnerAdventureP4Caballero/Assets/Scripts/ActualPlayerController.cs
+++ 2DBeginnerAdventureP4Caballero/Assets/Scripts/ActualPlayerController.cs
@@ -5,9 +5,15 @@ using UnityEngine.InputSystem;
 
 public class ActualPlayerController : MonoBehaviour
 {
-    public float speed = 3.0
+    public float speed = 3.0f;
+    public float timeInvincable = 2;
     public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincableTimer;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -17,6 +23,7 @@ public class ActualPlayerController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+
     }
 
     // Update is called once per frame
@@ -24,6 +31,15 @@ public class ActualPlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincableTimer -= Time.deltaTime;
+            if(invincableTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -35,8 +51,17 @@ public class ActualPlayerController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincableTimer = timeInvincable;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
